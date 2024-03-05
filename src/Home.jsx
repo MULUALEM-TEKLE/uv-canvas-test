c5da2cb223dd6773ce2461a493b42e169eab9285
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2024-03-05 18:22:12 +0300

    implemented names

--- src/Home.jsx
+++ src/Home.jsx
@@ -16,6 +16,15 @@ export function Home() {
 		Opulento,
 		Zenitho,
 	]
+	// had to create a separate arrays for names because for some reason when deploying on vercel names get codified
+	const componentNames = [
+		"Lumiflex",
+		"Novatrix",
+		"Velustro",
+		"Tranquiluxe",
+		"Opulento",
+		"Zenitho",
+	]
 	return (
 		<>
 			<div className="home">
@@ -23,7 +32,7 @@ export function Home() {
 					return (
 						<div key={index} className={`uv-component uv-component-${index}`}>
 							<Component />
-							<div className="component-name">{Component.name}</div>
+							<div className="component-name">{componentNames[index]}</div>
 						</div>
 					)
 				})}
